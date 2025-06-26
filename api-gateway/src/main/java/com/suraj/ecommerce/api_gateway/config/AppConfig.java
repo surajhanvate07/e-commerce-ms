8367b5819d1653136e9b953ede962530f8892196
Suraj Hanvate <surajhanvate999@gmail.com>
2025-06-26 08:41:18 +0530

    Added zipkin and micrometer for tracing of requests

--- api-gateway/src/main/java/com/suraj/ecommerce/api_gateway/config/AppConfig.java
+++ api-gateway/src/main/java/com/suraj/ecommerce/api_gateway/config/AppConfig.java
@@ -1,19 +1,13 @@
-package com.suraj.ecommerce.order_service.config;
+package com.suraj.ecommerce.api_gateway.config;
 
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
 
 	@Bean
 	public Capability capability(final MeterRegistry registry) {
